7fb583291aef70e9120fd0b31f3c7d25e9d73f11
Pascal Honegger <pascal.honegger@bluewin.ch>
2017-09-10 19:55:30 +0200

    Kleine Änderungen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,24 +2,8 @@ import { Component, isDevMode } from '@angular/core';
 
 @Component({
   selector: 'app',
-  styleUrls: ['./app.component.css'],
-  template: `
-    <nav>
-      <a [routerLink]=" ['./'] ">Start</a>
-      <a [routerLink]=" ['./rechner'] ">Rechner</a>
-      <a [routerLink]=" ['./impressum'] ">Impressum</a>
-    </nav>
-
-    <main>
-      <router-outlet></router-outlet>
-    </main>
-
-    <!--
-    <footer>
-      <ng2-adsense></ng2-adsense>
-    </footer>
-    -->
-  `
+  styleUrls: [ './app.component.css' ],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   constructor() {
