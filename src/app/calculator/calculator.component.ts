237ee4868dbd57922c9eb950afca0be45c70d3b0
Pascal Honegger <pascal.honegger@bluewin.ch>
2017-10-10 13:01:56 +0200

    Ich esse Fleisch => Ich esse kein Fleisch

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -3,7 +3,7 @@ import { SourceService } from './../services/source.service';
 import { CustomTitleService } from './../services/custom-title.service';
 import { Component } from '@angular/core';
 
-const eatsMeatKey = 'eatsMeat';
+const eatsNoMeatKey = 'eatsNoMeat';
 
 @Component({
   selector: 'calculator',
@@ -11,24 +11,30 @@ const eatsMeatKey = 'eatsMeat';
   templateUrl: './calculator.component.html'
 })
 export class CalculatorComponent {
-  public eatsMeatFormControl = new FormControl(undefined, Validators.required);
+  public set eatsNoMeat(value: boolean) {
+    if (this._eatsNoMeat === value) {
+      return;
+    }
+
+    this._eatsNoMeat = value;
+    localStorage.setItem(eatsNoMeatKey, value.toString());
+  }
+
+  public get eatsNoMeat(): boolean {
+    return this._eatsNoMeat;
+  }
+
+  private _eatsNoMeat: boolean;
 
   constructor(public sourceService: SourceService, title: CustomTitleService) {
     title.detailTitle = 'Rechner';
     title.description = 'Berechnen Sie, wie Ihr Fleischkonsum die Umwelt belastet';
 
-    const item = localStorage.getItem(eatsMeatKey);
+    const item = localStorage.getItem(eatsNoMeatKey);
     if (item === true.toString()) {
-      this.eatsMeatFormControl.setValue(true);
-    } else if (item === false.toString()) {
-      this.eatsMeatFormControl.setValue(false);
+      this._eatsNoMeat = true;
+    } else {
+      this._eatsNoMeat = false;
     }
-
-    this.eatsMeatFormControl.valueChanges.subscribe(
-      (val) => {
-        if (val !== undefined) {
-          localStorage.setItem(eatsMeatKey, val.toString());
-        }
-      });
   }
 }
